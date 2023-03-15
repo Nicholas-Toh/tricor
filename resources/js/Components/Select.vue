54b91265db9b1b5676ecbe6a959cb0e34a0f0e4d
nicholas <nicholas@boostorder.com>
2023-03-15 09:59:41 +0800

    Make select component nullable

--- resources/js/Components/Select.vue
+++ resources/js/Components/Select.vue
@@ -12,6 +12,14 @@ const props = defineProps({
     },
     labels: {
         type: Object,
+    },
+    nullable: {
+        type: Boolean,
+        default: true,
+    },
+    nullText: {
+        type: String,
+        default: ""
     }
 });
 
@@ -33,6 +41,7 @@ const getOption = (option) => props.labels[option] ?? option
 <template>
     <select class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)" ref="input">
+        <option v-if="nullable" :value="null">{{ nullText }}</option>
         <option v-for="option in props.options" :key="option" :value="option">{{ getOption(option) }}</option>
     </select>
 </template>
